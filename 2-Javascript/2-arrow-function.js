e7cb35bf2533d29629484a9681814be82943c1fc
vaarun18 <varunsadananda108@gmail.com>
2022-11-12 11:59:34 +0530

    added second module

---
+++ 2-Javascript/2-arrow-function.js
@@ -0,0 +1,26 @@
+// function myFunc(){
+//     ...
+// }
+
+// const myFunc=()=>{
+//     ...
+// }
+
+//Normal function
+// function printMyName(name) {
+//   console.log(name);
+// }
+
+//Arrow Function
+
+const printMyName = (name) => {
+  console.log(name);
+};
+
+printMyName("max");
+
+const multiply = (number) => {
+  return 2 * number;
+};
+
+console.log(multiply(2));
